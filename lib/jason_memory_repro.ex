e1da43cfc0aca60b32bf9c4669d46bcc75bd45d9
mmmries <riesmmm@gmail.com>
2021-05-25 15:18:39 -0700

    initial

---
+++ lib/jason_memory_repro.ex
@@ -0,0 +1,18 @@
+defmodule JasonMemoryRepro do
+  @moduledoc """
+  Documentation for `JasonMemoryRepro`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JasonMemoryRepro.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jason_memory_repro_test.exs
@@ -0,0 +1,8 @@
+defmodule JasonMemoryReproTest do
+  use ExUnit.Case
+  doctest JasonMemoryRepro
+
+  test "greets the world" do
+    assert JasonMemoryRepro.hello() == :world
+  end
+end
